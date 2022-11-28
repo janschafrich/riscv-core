27427b625d6324267dcd3fb1fa24ce332fdd9ad5
Jan-Eric Schäfrich <74378951+Centurio-Macro@users.noreply.github.com>
2022-11-28 21:56:06 +0100

    added support for instructions

--- riscv_core.v
+++ riscv_core.v
@@ -58,11 +58,12 @@ module instruction_memory(
 	end
 	always @*
 		case(addr)	// lookup table - instructions from Steven Hoover RISC-V tutorial https://github.com/stevehoover/LF-Building-a-RISC-V-CPU-Core.git
-			32'h0:	rom_data = 32'b0000_0001_0101_0000_0000_0000_1001_0011;		// (I) ADDI x1, x0, 10101
-			32'h4:	rom_data = 32'b0000_0000_0111_0000_0000_0000_0001_0011;		// (I) ADDI x2, x0, 111
-			32'h8:	rom_data = 32'b1111_1111_1100_0000_0000_0001_1001_0011;		// (I) ADDI x3, x0, 111111111100
-			32'hc:	rom_data = 32'b0000_0101_1100_0000_1111_0010_1001_0011;		// (I) ANDI x5, x1, 1011100
-			32'h10:	rom_data = 32'b0000_0001_0101_0010_1100_0010_1001_0011;		// (I) XORI x6, x6, 10101
+			//                                            imm		    	     rs1   funct3   rd      opcode
+			32'h0:	rom_data = 32'b0000_0001_0101_00000_000_00001_0010011;		// (I) ADDI x1, x0, 10101
+			32'h4:	rom_data = 32'b0000_0000_0111_00000_000_00010_0010011;		// (I) ADDI x2, x0, 111
+			32'h8:	rom_data = 32'b1111_1111_1100_00000_000_00011_0010011;		// (I) ADDI x3, x0, 111111111100
+			32'hc:	rom_data = 32'b0000_0101_1100_00001_111_00101_0010011;		// (I) ANDI x5, x1, 1011100
+			32'h10:	rom_data = 32'b0000_0001_0101_00101_100_00101_0010011;		// (I) XORI x5, x5, 10101
 			32'h14:	rom_data = 32'b0000_0000_0010_0000_1111_0101_0011_0011;		// Cycle 13 (R) AND r10, x1, x2
 			32'h18:	rom_data = 32'b0000_0000_0000_0000_0000_1001_1011_0111;		// Cycle 31 (U) LUI x19, 0
 			32'h1c:	rom_data = 32'b0000_0000_0100_0000_0000_1100_1110_1111;		// Cycle 44 (J) JAL x25, 10
@@ -77,15 +78,14 @@ module instruction_decode(
 	input [31:0]instr,
 	output reg is_r_instr, is_i_instr, is_s_instr, is_b_instr, is_u_instr, is_j_instr,
 	output reg [6:0]opcode,
-	output reg[4:0]rd,			// destination register
+	output reg[4:0]rd,	rs1, rs2,		// destination register, source register 1, source register 2
 	output reg[2:0]funct3,
-	output reg[4:0]rs1,		// source register 1
-	output reg[4:0]rs2,			// source register 1
 	output reg[31:0]imm, //src1_value, src2_value, dest_value,		// immediate value (= operand that is decoded inside the instruction)
 	output reg rd_valid, funct3_valid, rs1_valid, rs2_valid, imm_valid,
 	output reg [10:0]dec_bits,	// instruction identification
 	output reg is_addi, is_add, is_beq, is_bne, is_blt, is_bge, is_bltu, is_bgeu, is_lui, is_auipc, is_jal, is_jalr,
-	output reg is_xori, is_ori, is_andi, is_and
+	output reg is_xori, is_ori, is_andi, is_and,
+	output reg is_slti, is_sltiu
 	);
 
 	initial begin
@@ -144,17 +144,15 @@ module instruction_decode(
 			imm <= 32'b0;	
 	end
 
-	always @(instr or rd_valid or funct3_valid or rs1_valid or rs2_valid) begin
+	always @(*) begin
+		//instr or rd_valid or funct3_valid or rs1_valid or rs2_valid
 		//determine instruction field values
 		opcode 	<= instr[6:0];
-		if (rd_valid) 
-			rd		<= instr[11:7];		// loaded for S-type instructions as well, but not used
-		else if(funct3_valid)
-			funct3 	<= instr[14:12];
-		else if(rs1_valid)
-			rs1		<= instr[19:15];
-		else if(rs2_valid)
-			rs2 		<= instr[24:20];
+		#0.1		// somehow needed to extract the values
+		rd 		<= rd_valid ? 	instr[11:7] : 5'b0;		// loaded for S-type instructions as well, but not used
+		funct3 	<= funct3_valid ? 	instr[14:12] : 3'b0;
+		rs1 		<= rs1_valid ?	instr[19:15] : 5'b0;
+		rs2 		<= rs2_valid ? 	instr[24:20]: 5'b0;
 	end
 
 	always @(instr or funct3 or opcode) begin
@@ -167,21 +165,20 @@ module instruction_decode(
   		is_blt 	<= dec_bits[9:0] 		== 10'b_100_1100011;		//branch less than
   		is_bge 	<= dec_bits[9:0] 		== 10'b_101_1100011;		//branch greater or equal
   		is_bltu 	<= dec_bits[9:0] 		== 10'b_110_1100011;		//branch less than unsigned
-  		is_bgeu 	<= dec_bits[9:0] 		== 10'b_111_1100011;		//branch greater than unsigned		
-  		is_addi 	<= dec_bits[9:0] 		== 10'b_000_0010011;		//add immediate		
+  		is_bgeu 	<= dec_bits[9:0] 		== 10'b_111_1100011;		//branch greater than unsigned			
   		is_add 	<= dec_bits[10:0]		== 11'b0_000_0110011;		//add	
 		is_lui 	<= dec_bits[6:0]		== 11'b_0110111; 			// load upper immediate
   		is_auipc 	<= dec_bits[6:0]		== 11'b_0010111; 			// add upper immediate to pc
    		is_jal 	<= dec_bits[6:0] 		== 11'b_1101111; 			// jump and link (offset as immediate)
-   		is_jalr 	<= dec_bits[6:0]		== 11'b_1100111; // jump and link register (offset to be calculated)
-   /*
-   $is_load = $dec_bits ==? 11'bx_xxx_000011; // dont distinguish between indiviual load instructions
-   $is_addi = $dec_bits ==? 11'bx_000_0010011; // add immediate
-   $is_slti = $dec_bits ==? 11'bx_010_0010011; // set less than immediate
-   $is_sltiu = $dec_bits ==? 11'bx_011_0010011; // set less than immediate unsigned
-   */
+   		is_jalr 	<= dec_bits[6:0]		== 11'b_1100111; 			// jump and link register (offset to be calculated)
+
+   //$is_load = $dec_bits ==? 11'bx_xxx_000011; // dont distinguish between indiviual load instructions
+   		is_addi 	<= dec_bits[9:0]		== 11'b_000_0010011; 		// add immediate
+   		is_slti 	<= dec_bits[9:0]		== 11'b_010_0010011; 		// set less than immediate
+   		is_sltiu 	<= dec_bits[9:0]		== 11'b_011_0010011; 		// set less than immediate unsigned
+
 			// instructions are not correctly decoded. 
-			//funct3 is not extracted
+			//rd, rs1, funct3 are not extracted			
 			//dec_bits is not correctly constructed
 			
 
@@ -247,6 +244,7 @@ module arithmetic_logic_unit(
 	input is_addi, is_add, 
 	input is_beq, is_bne, is_blt, is_bge, is_bltu, is_bgeu, is_lui, is_auipc, is_jal, is_jalr,
 	input is_xori, is_ori, is_andi, is_and,
+	input is_slti, is_sltiu,
 	output reg[31:0] result,
 	output reg taken_br
 );
@@ -268,6 +266,10 @@ module arithmetic_logic_unit(
 				is_add  	? src1_value + src2_value :
 				taken_br	? pc + imm :									// RISC-V spec p.15 / p.17
 				is_jalr	? src1_value + imm :							// RISC-V spec p. 16
+				is_slti	? (src1_value[31] == imm[31]) ? 			// RISC-V spec p. 13
+							{31'b0, src1_value < imm} : 
+							{31'b0, src1_value[31]} :
+				is_sltiu	? {31'b0, src1_value < imm} :				// 0000000000000000000000 comp
 				32'b0; // default
 		end
 	end
